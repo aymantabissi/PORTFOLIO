bbd88711726937ee96f6e055805873d01e2b01a1
aymantabissi <aymanraisse7@gmail.com>
2025-08-17 03:36:26 +0200

    Fix MyWork import path - correct folder case

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 import Hero from "./components/Hero/Hero.jsx"; // H in Hero must match folder
 import About from "./components/About/About.jsx";
 import Services from "./components/Services/Services.jsx";
-import MyWork from "./components/MyWork/MyWork.jsx";
+import MyWork from "./components/Mywork/MyWork.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
